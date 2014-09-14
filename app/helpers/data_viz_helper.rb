3b419ec00048e981051ccc4629bd79b852115567
gpattus <pattusg@gmail.com>
2014-09-14 11:39:43 +0200

    [data-viz] change display_data_viz method in data_viz_helper to create a div with right id

--- app/helpers/data_viz_helper.rb
+++ app/helpers/data_viz_helper.rb
@@ -1,5 +1,12 @@
 module DataVizHelper
-   def helloworld
-       "hello world!"
-   end
+  def helloworld
+    "hello world!"
+  end
+
+  def display_data_viz
+    data_model_json = DataModelViz.generate_json
+
+    content_tag(:div, id: "data_viz_model", data: {model: data_model_json}) do
+    end
+  end
 end

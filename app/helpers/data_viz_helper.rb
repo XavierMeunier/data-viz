7e2cdca10cb334e7a2fb19a9d457f55c18831613
XavierMeunier <meunier.xavier22@gmail.com>
2014-11-18 19:38:38 +0100

    [Helper] Remove call to Module in helper

--- app/helpers/data_viz_helper.rb
+++ app/helpers/data_viz_helper.rb
@@ -1,15 +1,9 @@
 module DataVizHelper
-  def helloworld
-    "hello world!"
-  end
 
   def display_data_viz
-    data_model_json = DataModelViz.generate_json
-
-
     content_tag(:div, id: "dataviz_container") do
 
-      concat(content_tag(:div, id: "dataviz_model", data: {model: data_model_json}) do
+      concat(content_tag(:div, id: "dataviz_model", data: {model: @data_model_json}) do
       end)
       
       concat(link_to("#", {class: "dataviz_btn dataviz_paper_raise", id: "save_position"}) do
